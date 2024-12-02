93db0cff65b1356c0df4fe1d489eb908d56760d2
ishaanchip <ishaanchiplunkar@gmail.com>
2024-12-01 20:51:22 -0500

    finding ip log

--- index.js
+++ index.js
@@ -30,3 +30,13 @@ const port = process.env.PORT || 4000;
 const server = app.listen(port, ()=>{
     console.log(`Server is running: PORT ${port}`)
 })
+
+const axios = require('axios');
+
+axios.get('https://api64.ipify.org?format=json')
+  .then(response => {
+    console.log('Server outbound IP address:', response.data.ip);
+  })
+  .catch(error => {
+    console.error('Error fetching IP address:', error);
+  });
